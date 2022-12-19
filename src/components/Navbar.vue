f9af69eb3399af2f9e2ab101fe7c5ee3bd9c08e8
Melliim <ibrahim_meleha@hotmail.com>
2022-12-19 14:56:01 +0100

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">My Habit App</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <router-link class="nav-link" to="/">Home</router-link>
+          <router-link class="nav-link" to="/about">About</router-link>
+          <router-link class="nav-link" to="/habits">Habits</router-link>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Logs
+            </a>
+            <ul class="dropdown-menu">
+              <router-link class="dropdown-item" to="/daily">daily</router-link>
+              <router-link class="dropdown-item" to="/weekly">weekly</router-link>
+              <router-link class="dropdown-item" to="/monthly">monthly</router-link>
+              <router-link class="dropdown-item" to="/annually">annually</router-link>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Habits.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Welcome to Habits</h1>
+</template>
+
+<script>
+export default {
+  name: 'Habits'
+}
+</script>
+
+<style scoped>
+
+</style>
